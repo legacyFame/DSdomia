540a8b52215a49b5aaf1db1b2711a31867d00c52
Nasfame <laciferin@gmail.com>
2021-02-16 18:48:52 +0530

    Fixed Infinix to Postfix ....

---
+++ cmake-build-debug/CMakeFiles/DSdomia.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSdomia.dir/main.c.obj"
+  "DSdomia.exe"
+  "DSdomia.exe.manifest"
+  "DSdomia.pdb"
+  "libDSdomia.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DSdomia.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- infinixToPostfix.c
+++ infinixToPostfix.c
@@ -3,15 +3,16 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
+
 /** Infinix to postfix
  * author - Hiro*/
 int main() {
     bool checkoperator(char op);
     int priority(char c);
     int top = -1, i = 0;
-    char *stk,op;
-    stk = (char *) malloc (100);
-    char *exp = "12 + 2 * 3";
+    char *stk,*exp, op;
+    stk = (char *) malloc(100);
+    exp = "12 + 2 * 3";
     printf("Infinix expresion %s\n", exp);
     while (i < strlen(exp)) {
         op = exp[i];
@@ -23,21 +24,21 @@ int main() {
                 printf("%c ", stk[top]);
                 top--;
             }
-            if(op==')'|| op=='(')
+            if (op == ')' || op == '(')
                 continue;
             top++;
             stk[top] = op;
         } else if (isdigit(op)) {
             while (isdigit(op)) {
-                printf("%c",op); // for multi digit nos
+                printf("%c", op); // for multi digit nos
                 op = exp[i];
                 i++;
             }
             printf(" ");
         }
     }
-    while(top>-1){
-        printf("%c ",stk[top]);
+    while (top > -1) {
+        printf("%c ", stk[top]);
         top--;
     }
 }
@@ -55,6 +56,8 @@ bool checkoperator(char op) { // Not necessary isdigit should do everything .. j
 }
 
 int priority(char c) {
-    int priority = (c=='+')?1:((c=='-')?1:((c=='*')?2:((c=='^')?3:((c=='/')?2:((c==')')?9:0)))));
+    int priority = (c == '+') ? 1 : ((c == '-') ? 1 : ((c == '*') ? 2 : ((c == '^') ? 3 : ((c == '/') ? 2 : ((c == ')')
+                                                                                                             ? 9
+                                                                                                             : 0)))));
     return priority;
 }
